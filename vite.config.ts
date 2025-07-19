7b73536fe32e50e056c3017109c78c79d44cafba
Y0U5S3F <akim25486@gmail.com>
2025-07-19 11:10:50 +0100

    ekhdem

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/GlossedUp/',
+  base: '/', // Use '/' for root deployment (custom domain)
 })
